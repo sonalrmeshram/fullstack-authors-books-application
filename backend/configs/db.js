41f324f2dfe4f81c8e54d00c7df2c9fbe7526c96
sonalrmeshram <sonalrmeshram@gmail.com>
2025-11-07 08:59:26 +0530

    Update db.js

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-   host: 'localhost',
+   host: 'database-1.ckn4c840kn2f.us-east-1.rds.amazonaws.com',
    port: '3306',
-   user: 'root',
-   password: '12345678',
+   user: 'admin',
+   password: 'Sonal45815',
    database: 'react_node_app'
 });
 
